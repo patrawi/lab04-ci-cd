6336dbf0fb12490b290ebdc8ae6d875d781a6384
Patrawi27 <67262217+patrawi@users.noreply.github.com>
2022-10-08 01:08:05 +0700

    test: redis connection

--- src/configure.js
+++ src/configure.js
@@ -3,5 +3,6 @@ const config_default = require('../conf/default')
 
 module.exports = (config_custom = {}) => {
   const config = mixme.merge(config_default, config_custom)
+  console.log(config);
   return config
 }
