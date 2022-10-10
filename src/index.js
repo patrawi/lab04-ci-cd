1f5983e75537418b949ca86f69d2748bb446b498
Patrawi27 <67262217+patrawi@users.noreply.github.com>
2022-10-11 06:56:04 +0700

    feat: main

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const config = configure()
 // Creates a new Redis client
 // If REDIS_HOST is not set, the default host is localhost
 // If REDIS_PORT is not set, the default port is 6379
-console.log(config);
+console.log(config.redis.host);
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
